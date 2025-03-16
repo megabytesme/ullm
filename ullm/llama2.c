147e44ec98f792d1a6066a3a912f9be8a8632a1e
Andrew Rossignol <andrew.rossignol@gmail.com>
2025-03-16 09:30:36 -0700

    Remove last `exit` call from llama2.

--- ullm/llama2.c
+++ ullm/llama2.c
@@ -439,127 +439,130 @@ int str_lookup(const char *str, UllmLlama2TokenIndex *sorted_vocab, int vocab_si
   return res != NULL ? res->id : -1;
 }
 
-void encode(UllmLlama2State* state, const char *text, int8_t bos, int8_t eos, int *tokens, int *n_tokens) {
-    // encode the string text (input) into an upper-bound preallocated tokens[] array
-    // bos != 0 means prepend the BOS token (=1), eos != 0 means append the EOS token (=2)
-    if (text == NULL) { fprintf(stderr, "cannot encode NULL text\n"); exit(EXIT_FAILURE); }
+static void UllmLlama2Encode(const UllmLlama2RunConfig* config,
+    UllmLlama2State* state, int8_t bos, int8_t eos, int *tokens,
+    int *n_tokens) {
+  // encode the string text (input) into an upper-bound preallocated tokens[] array
+  // bos != 0 means prepend the BOS token (=1), eos != 0 means append the EOS token (=2)
+  int32_t vocab_size = state->transformer.config.vocab_size;
+  UllmLlama2Tokenizer* t = &state->tokenizer;
+  if (t->sorted_vocab == NULL) {
+    // lazily UllmMemoryAlloc and sort the vocabulary
+    t->sorted_vocab = UllmMemoryAlloc(vocab_size * sizeof(UllmLlama2TokenIndex));
+    for (int i = 0; i < vocab_size; i++) {
+        t->sorted_vocab[i].str = t->vocab[i];
+        t->sorted_vocab[i].id = i;
+    }
+    qsort(t->sorted_vocab, vocab_size, sizeof(UllmLlama2TokenIndex), compare_tokens);
+  }
 
-    int32_t vocab_size = state->transformer.config.vocab_size;
-    UllmLlama2Tokenizer* t = &state->tokenizer;
-    if (t->sorted_vocab == NULL) {
-        // lazily UllmMemoryAlloc and sort the vocabulary
-        t->sorted_vocab = UllmMemoryAlloc(vocab_size * sizeof(UllmLlama2TokenIndex));
-        for (int i = 0; i < vocab_size; i++) {
-            t->sorted_vocab[i].str = t->vocab[i];
-            t->sorted_vocab[i].id = i;
-        }
-        qsort(t->sorted_vocab, vocab_size, sizeof(UllmLlama2TokenIndex), compare_tokens);
+  // create a temporary buffer that will store merge candidates of always two consecutive tokens
+  // *2 for concat, +1 for null terminator +2 for UTF8 (in case max_token_length is 1)
+  char* str_buffer = UllmMemoryAlloc((t->max_token_length*2 +1 +2) * sizeof(char));
+  size_t str_len = 0;
+
+  // start at 0 tokens
+  *n_tokens = 0;
+
+  // add optional BOS (=1) token, if desired
+  if (bos) {
+    tokens[(*n_tokens)++] = 1;
+  }
+
+  // add_dummy_prefix is true by default
+  // so prepend a dummy prefix token to the input string, but only if text != ""
+  // TODO: pretty sure this isn't correct in the general case but I don't have the
+  // energy to read more of the sentencepiece code to figure out what it's doing
+  if (config->prompt[0] != '\0') {
+    int dummy_prefix = str_lookup(" ", t->sorted_vocab, vocab_size);
+    tokens[(*n_tokens)++] = dummy_prefix;
+  }
+
+  // Okay UTF-8 time. This will get messy. Here is the reference from Wikipedia:
+  // Code point ↔ UTF-8 conversion
+  // First code point	Last code point	Byte 1	Byte 2	Byte 3	Byte 4
+  // U+0000	U+007F	    0xxxxxxx
+  // U+0080	U+07FF	    110xxxxx	10xxxxxx
+  // U+0800	U+FFFF	    1110xxxx	10xxxxxx	10xxxxxx
+  // U+10000	U+10FFFF    11110xxx	10xxxxxx	10xxxxxx	10xxxxxx
+
+  // process the raw (UTF-8) byte sequence of the input string
+  for (const char *c = config->prompt; *c != '\0'; c++) {
+    // reset buffer if the current byte is ASCII or a leading byte
+    // 0xC0 is 11000000, so (*c & 0xC0) keeps the first 2 bits and zeros the rest
+    // 0x80 is 10000000
+    // in UTF-8, all continuation bytes start with "10" in first two bits
+    // so in English this is: "if this byte is not a continuation byte"
+    if ((*c & 0xC0) != 0x80) {
+      // this byte must be either a leading byte (11...) or an ASCII char (0x...)
+      // => reset our location, as we're starting a new UTF-8 codepoint
+      str_len = 0;
     }
 
-    // create a temporary buffer that will store merge candidates of always two consecutive tokens
-    // *2 for concat, +1 for null terminator +2 for UTF8 (in case max_token_length is 1)
-    char* str_buffer = UllmMemoryAlloc((t->max_token_length*2 +1 +2) * sizeof(char));
-    size_t str_len = 0;
+    // append the current byte to the buffer
+    str_buffer[str_len++] = *c; // ++ is post-increment, incremented after this line
+    str_buffer[str_len] = '\0';
 
-    // start at 0 tokens
-    *n_tokens = 0;
-
-    // add optional BOS (=1) token, if desired
-    if (bos) tokens[(*n_tokens)++] = 1;
-
-    // add_dummy_prefix is true by default
-    // so prepend a dummy prefix token to the input string, but only if text != ""
-    // TODO: pretty sure this isn't correct in the general case but I don't have the
-    // energy to read more of the sentencepiece code to figure out what it's doing
-    if (text[0] != '\0') {
-        int dummy_prefix = str_lookup(" ", t->sorted_vocab, vocab_size);
-        tokens[(*n_tokens)++] = dummy_prefix;
+    // while the next character is a continuation byte, continue appending
+    // but if there are too many of them, just stop to avoid overruning str_buffer size.
+    if ((*(c+1) & 0xC0) == 0x80 && str_len < 4) {
+      continue;
     }
 
-    // Okay UTF-8 time. This will get messy. Here is the reference from Wikipedia:
-    // Code point ↔ UTF-8 conversion
-    // First code point	Last code point	Byte 1	Byte 2	Byte 3	Byte 4
-    // U+0000	U+007F	    0xxxxxxx
-    // U+0080	U+07FF	    110xxxxx	10xxxxxx
-    // U+0800	U+FFFF	    1110xxxx	10xxxxxx	10xxxxxx
-    // U+10000	U+10FFFF    11110xxx	10xxxxxx	10xxxxxx	10xxxxxx
+    // ok c+1 is not a continuation byte, so we've read in a full codepoint
+    int id = str_lookup(str_buffer, t->sorted_vocab, vocab_size);
 
-    // process the raw (UTF-8) byte sequence of the input string
-    for (const char *c = text; *c != '\0'; c++) {
+    if (id != -1) {
+      // we found this codepoint in vocab, add it as a token
+      tokens[(*n_tokens)++] = id;
+    } else {
+      // byte_fallback encoding: just encode each byte as a token
+      // +3 is here because the first 3 vocab elements are <unk>, <s>, </s>
+      // so the individual bytes only start at index 3
+      for (int i=0; i < str_len; i++) {
+        tokens[(*n_tokens)++] = (unsigned char)str_buffer[i] + 3;
+      }
+    }
+    str_len = 0; // protect against a sequence of stray UTF8 continuation bytes
+  }
 
-        // reset buffer if the current byte is ASCII or a leading byte
-        // 0xC0 is 11000000, so (*c & 0xC0) keeps the first 2 bits and zeros the rest
-        // 0x80 is 10000000
-        // in UTF-8, all continuation bytes start with "10" in first two bits
-        // so in English this is: "if this byte is not a continuation byte"
-        if ((*c & 0xC0) != 0x80) {
-            // this byte must be either a leading byte (11...) or an ASCII char (0x...)
-            // => reset our location, as we're starting a new UTF-8 codepoint
-            str_len = 0;
-        }
+  // merge the best consecutive pair each iteration, according the scores in vocab_scores
+  while (1) {
+    float best_score = -1e10;
+    int best_id = -1;
+    int best_idx = -1;
 
-        // append the current byte to the buffer
-        str_buffer[str_len++] = *c; // ++ is post-increment, incremented after this line
-        str_buffer[str_len] = '\0';
-
-        // while the next character is a continuation byte, continue appending
-        // but if there are too many of them, just stop to avoid overruning str_buffer size.
-        if ((*(c+1) & 0xC0) == 0x80 && str_len < 4) {
-            continue;
-        }
-
-        // ok c+1 is not a continuation byte, so we've read in a full codepoint
-        int id = str_lookup(str_buffer, t->sorted_vocab, vocab_size);
-
-        if (id != -1) {
-            // we found this codepoint in vocab, add it as a token
-            tokens[(*n_tokens)++] = id;
-        } else {
-            // byte_fallback encoding: just encode each byte as a token
-            // +3 is here because the first 3 vocab elements are <unk>, <s>, </s>
-            // so the individual bytes only start at index 3
-            for (int i=0; i < str_len; i++) {
-                tokens[(*n_tokens)++] = (unsigned char)str_buffer[i] + 3;
-            }
-        }
-        str_len = 0; // protect against a sequence of stray UTF8 continuation bytes
+    for (int i=0; i < (*n_tokens-1); i++) {
+      // check if we can merge the pair (tokens[i], tokens[i+1])
+      sprintf(str_buffer, "%s%s", t->vocab[tokens[i]], t->vocab[tokens[i+1]]);
+      int id = str_lookup(str_buffer, t->sorted_vocab, vocab_size);
+      if (id != -1 && t->vocab_scores[id] > best_score) {
+        // this merge pair exists in vocab! record its score and position
+        best_score = t->vocab_scores[id];
+        best_id = id;
+        best_idx = i;
+      }
     }
 
-    // merge the best consecutive pair each iteration, according the scores in vocab_scores
-    while (1) {
-        float best_score = -1e10;
-        int best_id = -1;
-        int best_idx = -1;
-
-        for (int i=0; i < (*n_tokens-1); i++) {
-            // check if we can merge the pair (tokens[i], tokens[i+1])
-            sprintf(str_buffer, "%s%s", t->vocab[tokens[i]], t->vocab[tokens[i+1]]);
-            int id = str_lookup(str_buffer, t->sorted_vocab, vocab_size);
-            if (id != -1 && t->vocab_scores[id] > best_score) {
-                // this merge pair exists in vocab! record its score and position
-                best_score = t->vocab_scores[id];
-                best_id = id;
-                best_idx = i;
-            }
-        }
-
-        if (best_idx == -1) {
-            break; // we couldn't find any more pairs to merge, so we're done
-        }
-
-        // merge the consecutive pair (best_idx, best_idx+1) into new token best_id
-        tokens[best_idx] = best_id;
-        // delete token at position best_idx+1, shift the entire sequence back 1
-        for (int i = best_idx+1; i < (*n_tokens-1); i++) {
-            tokens[i] = tokens[i+1];
-        }
-        (*n_tokens)--; // token length decreased
+    if (best_idx == -1) {
+      break; // we couldn't find any more pairs to merge, so we're done
     }
 
-    // add optional EOS (=2) token, if desired
-    if (eos) tokens[(*n_tokens)++] = 2;
+    // merge the consecutive pair (best_idx, best_idx+1) into new token best_id
+    tokens[best_idx] = best_id;
+    // delete token at position best_idx+1, shift the entire sequence back 1
+    for (int i = best_idx+1; i < (*n_tokens-1); i++) {
+      tokens[i] = tokens[i+1];
+    }
+    (*n_tokens)--; // token length decreased
+  }
 
-    UllmMemoryFree(str_buffer);
+  // add optional EOS (=2) token, if desired
+  if (eos) {
+    tokens[(*n_tokens)++] = 2;
+  }
+
+  UllmMemoryFree(str_buffer);
 }
 
 // ----------------------------------------------------------------------------
@@ -749,7 +752,7 @@ UllmStatus UllmLlama2Generate(const UllmLlama2RunConfig* config,
   int* prompt_tokens = (int*)UllmMemoryAlloc((strlen(config->prompt) + 3) * sizeof(int));
   // encode the (string) prompt into tokens sequence
   int num_prompt_tokens = 0;
-  encode(state, config->prompt, 1, 0, prompt_tokens, &num_prompt_tokens);
+  UllmLlama2Encode(config, state, 1, 0, prompt_tokens, &num_prompt_tokens);
   if (num_prompt_tokens == 0) {
       ULOGE("Prompt contains zero tokens");
       return ULLM_STATUS_INVALID_ARGUMENT;
