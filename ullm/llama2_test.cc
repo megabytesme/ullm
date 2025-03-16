35f7a18adad29f567f505d3d247670efd7245457
Andrew Rossignol <andrew.rossignol@gmail.com>
2025-03-16 09:55:33 -0700

    Remove printf and compare inference output directly.

--- ullm/llama2_test.cc
+++ ullm/llama2_test.cc
@@ -22,16 +22,30 @@ namespace ullm {
 namespace {
 
 void OutputHandler(const char* token, void* cookie) {
-  fprintf(stdout, "%s", token);
+  std::string* test_output = static_cast<std::string*>(cookie);
+  test_output->append(token);
 }
 
 TEST(UllmLlama2, Stories15M) {
+  const std::string expected_test_output = R"(The birds chirp. Where do they go?
+The birds flew around the sky, looking for something to do.
+The birds saw a big tree and flew over to it.
+The birds saw a big, red apple on the ground. It looked delicious.
+The birds flew down and picked up the apple.
+The birds flew back up to the tree and started to eat the apple.
+The apples were so delicious!
+The birds ate until they were full.
+The birds flew away, happy and full.
+)";
+
+  std::string test_output;
   UllmLlama2RunConfig run_config;
   UllmLlama2RunConfigInit(&run_config);
   run_config.checkpoint_path = "ullm/tinystories15M.bin";
   run_config.tokenizer_path = "ullm/tokenizer.bin";
   run_config.prompt = "The birds chirp. Where do they go?";
   run_config.output_callback = OutputHandler;
+  run_config.cookie = &test_output;
 
   UllmLlama2State state;
   UllmStatus status = UllmLlama2Init(&run_config, &state);
@@ -40,6 +54,8 @@ TEST(UllmLlama2, Stories15M) {
   ASSERT_EQ(status, ULLM_STATUS_OK);
   // TODO(aarossig): Add a test to verify that it can run repeatedly.
   UllmLlama2Deinit(&state);
+
+  EXPECT_EQ(test_output, expected_test_output);
 }
 
 }  // namespace
